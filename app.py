b434e83dfccc9690f52540ed1f9188e7f3d595e0
Harshinee Sriram <harshineesriram_sriram@srmuniv.edu.in>
2021-03-05 13:48:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import io
 import flask
 import numpy as np
+import tensorflow as tf
 from constants_api import (
     debug, use_reloader
 )
@@ -10,36 +11,31 @@ from prepare_image_api import (
 from PIL import Image
 
 app = flask.Flask(__name__)
-model = load_model()
 
+model = load_model()
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = {"success": False}
-    if flask.request.method == "POST":
-        if flask.request.files.get("image"):
-            # Read the image and use model to predict
-            # Store the predicted label and probability
-            label = ""
-            probability = 0
+    data = {"status_code": 400}
+    #if flask.request.method == "POST":
+    if flask.request.files.get("image"):
+        # Read the image and use model to predict
+        # Store the predicted label and probability
 
-            image = flask.request.files["image"].read()
-            image = Image.open(io.BytesIO(image))
-            image = prepare_image(image)
-            preds = model.predict(image)
+        image = flask.request.files["image"].read()
+        image = Image.open(io.BytesIO(image))
+        image = prepare_image(image)
+        preds = model.predict(image)
 
-            list_preds = list(preds)
-            if(np.argmax(preds) == 0):
-                label = "Unsafe"
-                probability = list_preds[0][0]
-            else:
-                label = "Safe"
-                probability = list_preds[0][1]
+        list_preds = list(preds)
 
-            data["predictions"] = []
-            r = {"label": label, "probability": float(probability)}
-            data["predictions"].append(r)
-            data["success"] = True
+        label = "Unsafe" if np.argmax(preds) == 0 else "Safe"
+        probability = list_preds[0][0] if np.argmax(preds) == 0 else list_preds[0][1]
+
+        data["predictions"] = []
+        r = {"label": label, "probability": float(probability)}
+        data["predictions"].append(r)
+        data["status_code"] = 200
 
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
@@ -47,5 +43,5 @@ def predict():
 
 if __name__ == "__main__":
     print(("Loading the model and starting the server. . ."))
-    load_model()
+    #load_model()
     app.run(debug=debug, use_reloader=use_reloader)
