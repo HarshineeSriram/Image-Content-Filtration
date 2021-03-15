135703ff823b35a9be5a29bccd3caca9a65218d9
Harshinee Sriram <harshineesriram_sriram@srmuniv.edu.in>
2021-03-15 11:10:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,24 +16,24 @@ model = load_model()
 @app.route("/predict", methods=["POST"])
 def predict():
     data = {}
-    if flask.request.files.get("image"):
+    #if flask.request.files.get("image"):
         # Read the image and use model to predict
         # Store the predicted label and probability
 
-        image = flask.request.files["image"].read()
-        image = Image.open(io.BytesIO(image))
-        image = prepare_image(image)
-        preds = model.predict(image)
+    image = flask.request.files["image"].read()
+    image = Image.open(io.BytesIO(image))
+    image = prepare_image(image)
+    preds = model.predict(image)
 
-        list_preds = list(preds)
+    list_preds = list(preds)
 
-        label = "Unsafe" if np.argmax(preds) == 0 else "Safe"
-        probability = list_preds[0][0] if np.argmax(preds) == 0 \
-            else list_preds[0][1]
+    label = "Unsafe" if np.argmax(preds) == 0 else "Safe"
+    probability = list_preds[0][0] if np.argmax(preds) == 0 \
+        else list_preds[0][1]
 
-        data["predictions"] = []
-        r = {"label": label, "probability": float(probability)}
-        data["predictions"].append(r)
+    data["predictions"] = []
+    r = {"label": label, "probability": float(probability)}
+    data["predictions"].append(r)
 
     # return the data dictionary as a JSON response and the status code
     return data, 200
